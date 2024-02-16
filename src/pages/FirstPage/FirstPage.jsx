e05d02e556f1f91d5c4693463c2d0d786cf7356e
astasiempre <ero1494651@gmail.com>
2024-02-16 23:03:33 +0530

    update files

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,15 +1,33 @@
-import { Container } from './FirstPage.styled';
-// import example from '../../assets/example.png';
-import { CardsGallery } from '../../components/Cards/CardsGallery';
+// import React from 'react';
+import { useSpring, animated } from 'react-spring';
+
+const centeredContentStyle = {
+  display: 'flex',
+  fontsize: '100px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+};
+
+const largeFontStyle = {
+  fontSize: '80px',
+  color: 'red',
+};
 
 const FirstPage = () => {
+   const animationProps = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 1000 },
+  });
+
   return (
-    <Container>
-      {/* <Title>First Page</Title> */}
-      <CardsGallery/>
-      {/* <StyledImage src={example} alt="Example" /> */}
-    </Container>
+    <animated.div style={{ ...centeredContentStyle, ...animationProps }}>
+      <h1 style={largeFontStyle}>Rental Car App</h1>
+    </animated.div>
   );
 };
 
 export default FirstPage;
+
+
--- src/pages/SecondPage/SecondPage.jsx
+++ src/pages/SecondPage/SecondPage.jsx
@@ -1,12 +1,13 @@
-import { Container, Block, Title, StyledLink } from './SecondPage.styled';
-
+// import { Container, Block, Title, StyledLink } from './SecondPage.styled';
+import { Container } from './SecondPage.styled';
+// import example from '../../assets/example.png';
+import { CardsGallery } from '../../components/Cards/CardsGallery';
 const SecondPage = () => {
   return (
     <Container>
-      <Block>
-        <Title>Second Page</Title>
-        <StyledLink to="/second/5">Half</StyledLink>
-      </Block>
+      {/* <Title>First Page</Title> */}
+      <CardsGallery />
+      {/* <StyledImage src={example} alt="Example" /> */}
     </Container>
   );
 };
